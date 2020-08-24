c4f9918f54b3476423149c33aa2291e80358ec86
Mauricevanbruggen <m.vanbruggen@gmail.com>
2020-08-24 16:05:22 +0200

    initial commit

---
+++ src/main/java/com/wines/springbootMongodb/SpringBootApplicationWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.wines.springbootMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationWithMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wines/springbootMongodb/SpringBootApplicationWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wines.springbootMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationWithMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
