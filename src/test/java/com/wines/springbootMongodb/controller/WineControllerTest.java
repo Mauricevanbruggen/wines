a1bca873ab5f2152fd029afc7c2d7e663199cd23
Mauricevanbruggen <m.vanbruggen@gmail.com>
2020-09-05 18:02:16 +0200

    add getAllWinesTest to ControllerTest

--- src/test/java/com/wines/springbootMongodb/controller/WineControllerTest.java
+++ src/test/java/com/wines/springbootMongodb/controller/WineControllerTest.java
@@ -1,16 +1,16 @@
 package com.wines.springbootMongodb.controller;
 
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wines.springbootMongodb.com.service.WineService;
 import com.wines.springbootMongodb.repository.WineRepository;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,7 +21,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import com.wines.springbootMongodb.model.Wine;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = WineController.class, excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
@@ -34,15 +36,48 @@ public class WineControllerTest {
   private WineRepository wineRepository;
 
   @MockBean
+  private WineService wineService;
+
+  @MockBean
   private MongoTemplate mongoTemplate;
 
-  private Wine wine;
+  private Wine wine1, wine2;
   private List<Wine> wineList;
+
+//  @BeforeEach
+//  public void before() {
+//    wine1.setId("1");
+//    wine1.setName("wine1");
+//
+//    wine2.setId("2");
+//    wine2.setName("wine2");
+//
+//    wineList = Arrays.asList(
+//        wine1,
+//        wine2
+//    );
+//  }
 
   @Test
   @DisplayName("getWines should return list of wines")
   public void getWinesShouldReturnList() throws Exception {
-    
+    Wine wine1 = new Wine();
+    wine1.setId("1");
+    wine1.setName("wine1");
+
+    Wine wine2 = new Wine();
+    wine2.setId("2");
+    wine2.setName("wine2");
+
+    wineList = Arrays.asList(
+        wine1,
+        wine2
+    );
+    given(wineService.getAllWines()).willReturn(wineList);
+
+    this.mockMvc.perform(get("/allwines/"))
+        .andExpect(status().isOk())
+        .andExpect( jsonPath("$.size()").value(wineList.size()));
   }
 
 
